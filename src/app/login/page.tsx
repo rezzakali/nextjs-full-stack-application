b5272f0e0b68680127f417c70ecb69c2a21415e0
rezzak ali <rezzak ali>
2023-07-22 13:35:10 +0530

    code push

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from 'next';
 import Login from '../../pages/Login/Login';
+
+export const metadata: Metadata = {
+  title: 'Nextjs Full-Stack - Login',
+  description: 'Login Page',
+};
 
 const page = () => {
   return <Login />;
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,4 +1,10 @@
 import Register from '@/pages/Register/Register';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Nextjs Full-Stack - Register',
+  description: 'Register Page',
+};
 
 const page = () => {
   return <Register />;
